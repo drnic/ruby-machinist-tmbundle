78b620dcbf4129060aa63ae01b1027f477a11eb7
Dr Nic Williams <drnicwilliams@gmail.com>
2009-03-13 11:45:46 +0800

    blueprint generator loads current rails app + distinguishes available + unavailable class names

--- Support/lib/machinist/mate/blueprint_helper.rb
+++ Support/lib/machinist/mate/blueprint_helper.rb
@@ -1,18 +1,44 @@
+require "fileutils"
+
 module Machinist
   module Mate
     class BlueprintHelper
-      attr_reader :current_class
-      def initialize(current_word)
-        @current_class = current_word
+      attr_reader :current_class, :current_class_name
+      def initialize(current_dir)
+        @current_dir = current_dir
       end
 
-      def generate_blueprint(stdout)
+      def generate_blueprint(current_word, stdout = $stdout)
+        load(File.join(project_root, "config", "environment.rb"))
+        methods_str = ""
+        begin
+          @current_class_name = current_word.classify
+          @current_class = @current_class_name.constantize
+        rescue NameError
+          methods_str = "# no model class #{current_class_name} found"
+        end
         stdout.puts <<-EOS.gsub(/^        /, '')
-        #{current_class}.blueprint do
-          $0
+        #{current_class_name}.blueprint do
+          #{methods_str}
         end
         
         EOS
+      end
+
+      def find_project_dir(current_dir)
+        return nil unless File.exists?(current_dir)
+        current_dir = File.expand_path(current_dir)
+        FileUtils.chdir(current_dir) do
+          parent_dir = File.expand_path("..")
+          return nil if parent_dir == current_dir
+          boot_file = File.join(current_dir, "config", "boot.rb")
+          return File.exists?(boot_file) ? current_dir : find_project_dir(parent_dir)
+        end
+      end
+
+      protected
+      def project_root
+        @project_root ||= find_project_dir(@current_dir)
       end
     end
   end
--- Support/spec/machinist/mate/blueprint_helper_spec.rb
+++ Support/spec/machinist/mate/blueprint_helper_spec.rb
@@ -5,23 +5,71 @@ module Machinist
   module Mate
     describe BlueprintHelper do
       before(:each) do
-        @helper = BlueprintHelper.new('BlogPost')
+        @current_dir = File.dirname(__FILE__) + "/../../../fixtures/some_rails_app/spec/"
+        @helper = BlueprintHelper.new(@current_dir)
       end
       describe "#generate_blueprint" do
-        before(:each) do
-          @stdout = StringIO.new
-          @blueprint = @helper.generate_blueprint(@stdout)
-          @stdout.flush
-          @stdout = @stdout.read
+        describe "for parent class BlogPost" do
+          before(:each) do
+            @stdout = StringIO.new
+            @blueprint = @helper.generate_blueprint('BlogPost', @stdout)
+            @stdout.rewind
+            @stdout = @stdout.read
+          end
+
+          it "should render a blueprint" do
+            @stdout.should == <<-EOS.gsub(/^            /, '')
+            BlogPost.blueprint do
+              title
+              body
+            end
+
+            EOS
+          end
         end
 
-        it "should render a blueprint" do
-          @stdout.should == <<-EOS.gsub(/^          /, '')
-          BlogPost.blueprint do
-            $0
+        describe "for child class BlogComment" do
+          before(:each) do
+            @stdout = StringIO.new
+            @blueprint = @helper.generate_blueprint('BlogComment', @stdout)
+            @stdout.rewind
+            @stdout = @stdout.read
           end
-          
-          EOS
+
+          it "should render a blueprint" do
+            @stdout.should == <<-EOS.gsub(/^            /, '')
+            BlogComment.blueprint do
+              blog_post
+              comment
+            end
+
+            EOS
+          end
+        end
+        
+        describe "for unknown class FooBar" do
+          before(:each) do
+            @stdout = StringIO.new
+            @blueprint = @helper.generate_blueprint('FooBar', @stdout)
+            @stdout.rewind
+            @stdout = @stdout.read
+          end
+
+          it "should render a blueprint" do
+            @stdout.should == <<-EOS.gsub(/^            /, '')
+            FooBar.blueprint do
+              # no model class FooBar found
+            end
+
+            EOS
+          end
+        end
+        
+        describe "#find_project_dir" do
+          it "should find config/boot.rb file" do
+            expected = File.expand_path(File.dirname(__FILE__) + '/../../../fixtures/some_rails_app')
+            expected.should == @helper.find_project_dir(@current_dir)
+          end
         end
       end
     end
